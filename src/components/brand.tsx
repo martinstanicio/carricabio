c574c4224560f6b9274f23db320ca5d84d3a6514
Martín Stanicio <martuytimo@gmail.com>
2024-12-30 15:38:51 -0300

    create placeholder brand component

---
+++ src/components/brand.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "@/app/icon.svg";
+import { cn } from "@/lib/utils";
+
+export default function Brand({
+  className,
+  ...props
+}: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+  return (
+    <Link
+      href="/"
+      className={cn("inline-block h-12 w-auto md:h-16", className)}
+      {...props}
+    >
+      <Image
+        src={logo}
+        alt="Logo de Carricabio"
+        sizes="auto"
+        className="h-full w-full"
+      />
+      <span className="sr-only">Carricabio</span>
+    </Link>
+  );
+}
