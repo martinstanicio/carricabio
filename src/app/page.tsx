03a848854e399ec6f0724f23a54567ae9ec851bd
Martín Stanicio <martuytimo@gmail.com>
2024-12-31 15:58:46 -0300

    add benefits to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Benefits from "@/components/benefits";
 import Hero from "@/components/hero";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Separator />
+      <div className="bg-secondary text-secondary-foreground">
+        <Benefits />
+      </div>
     </main>
   );
 }
