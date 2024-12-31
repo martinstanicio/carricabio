a6096320d7ad7677c99fd43c81e38e5827030eb2
Martín Stanicio <martuytimo@gmail.com>
2024-12-31 16:16:02 -0300

    get benefits from content folder

---
+++ content/benefits.ts
@@ -0,0 +1,29 @@
+import {
+  faChartSimple,
+  faCircleCheck,
+  faShield,
+  faUserGear,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const benefits = [
+  {
+    title: "Transparencia",
+    description: "Información clara sobre costos y rendimientos.",
+    icon: faCircleCheck,
+  },
+  {
+    title: "Seguridad",
+    description: "Protección avanzada para tus datos y operaciones.",
+    icon: faShield,
+  },
+  {
+    title: "Opciones",
+    description: "Inversiones en oro, crypto y acciones.",
+    icon: faChartSimple,
+  },
+  {
+    title: "Soporte",
+    description: "Expertos disponibles para resolver tus dudas.",
+    icon: faUserGear,
+  },
+];
--- src/components/benefits.tsx
+++ src/components/benefits.tsx
@@ -1,34 +1,12 @@
-import {
-  faChartSimple,
-  faCircleCheck,
-  faShield,
-  faUserGear,
-} from "@fortawesome/free-solid-svg-icons";
+import { benefits } from "~/benefits";
 import BenefitCard from "./benefit-card";
 
 export default function Benefits() {
   return (
     <section className="mx-auto grid max-w-4xl grid-cols-1 gap-4 px-4 py-12 sm:grid-cols-2 lg:max-w-6xl lg:grid-cols-4">
-      <BenefitCard
-        title="Transparencia"
-        description="Información clara sobre costos y rendimientos."
-        icon={faCircleCheck}
-      />
-      <BenefitCard
-        title="Seguridad"
-        description="Protección avanzada para tus datos y operaciones."
-        icon={faShield}
-      />
-      <BenefitCard
-        title="Opciones"
-        description="Inversiones en oro, crypto y acciones."
-        icon={faChartSimple}
-      />
-      <BenefitCard
-        title="Soporte"
-        description="Expertos disponibles para resolver tus dudas."
-        icon={faUserGear}
-      />
+      {benefits.map((benefit, i) => (
+        <BenefitCard key={i} {...benefit} />
+      ))}
     </section>
   );
 }
