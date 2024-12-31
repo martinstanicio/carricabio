cfe7eafdf5f8aaa5651c77520fbdededb43b27f5
Martín Stanicio <martuytimo@gmail.com>
2024-12-31 16:43:41 -0300

    move section styles to page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,13 @@ export default function Home() {
   return (
     <main>
       <Hero />
+
       <Separator />
+
       <div className="bg-secondary text-secondary-foreground">
-        <Benefits />
+        <section className="mx-auto grid max-w-4xl grid-cols-1 gap-4 px-4 py-12 sm:grid-cols-2 lg:max-w-6xl lg:grid-cols-4">
+          <Benefits />
+        </section>
       </div>
     </main>
   );
--- src/components/benefits.tsx
+++ src/components/benefits.tsx
@@ -2,11 +2,5 @@ import { benefits } from "~/benefits";
 import BenefitCard from "./benefit-card";
 
 export default function Benefits() {
-  return (
-    <section className="mx-auto grid max-w-4xl grid-cols-1 gap-4 px-4 py-12 sm:grid-cols-2 lg:max-w-6xl lg:grid-cols-4">
-      {benefits.map((benefit, i) => (
-        <BenefitCard key={i} {...benefit} />
-      ))}
-    </section>
-  );
+  return benefits.map((benefit, i) => <BenefitCard key={i} {...benefit} />);
 }
