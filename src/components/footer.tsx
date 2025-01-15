3fea8afce404a83458e3eaa03c7c8bef75383529
Martín Stanicio <martuytimo@gmail.com>
2025-01-15 12:22:51 -0300

    add border to header and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import SocialIcons from "./social-icons";
 
 export default function Footer() {
   return (
-    <footer className="dark bg-background text-foreground">
+    <footer className="dark border-t bg-background text-foreground">
       <div className="mx-auto max-w-4xl space-y-12 px-4 py-8">
         <div className="flex flex-col items-center justify-between gap-x-4 gap-y-12 sm:flex-row">
           <Brand />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./navbar";
 
 export default function Header() {
   return (
-    <header className="dark sticky top-0 z-50 flex items-center justify-between bg-background p-4 text-foreground">
+    <header className="dark sticky top-0 z-50 flex items-center justify-between border-b bg-background p-4 text-foreground">
       <Brand />
       <Navbar />
     </header>
